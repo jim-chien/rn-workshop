af37f4122fcb544ac20baee2f2013a33abb8454e
Matheus Vinicius Valenza <matheus.valenza@Matheuss-MacBook-Pro.local>
2024-03-20 17:26:33 -0300

    final commit

--- src/screens/Pokemon.tsx
+++ src/screens/Pokemon.tsx
@@ -1,8 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import { Image, Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import Pokeball from '../../assets/pokeball.png';
-import { colors, fontSize } from '../../theme';
+import {
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import Pokeball from "../../assets/pokeball.png";
+import { colors, fontSize } from "../../theme";
 
 export const Pokemon = ({ route, navigation }) => {
   const { name, url } = route.params;
@@ -17,21 +24,35 @@ export const Pokemon = ({ route, navigation }) => {
     <SafeAreaView>
       <View style={styles.pageContainer}>
         <Pressable
-          onPress={() => {}}
+          onPress={() => {
+            navigation.navigate("WebViewContainer", {
+              url: "https://rn-workshop.development.useorigin.com",
+            });
+          }}
           style={styles.pokeballButton}
         >
-          <Image source={Pokeball} style={styles.pokeballImage}/>
+          <Image source={Pokeball} style={styles.pokeballImage} />
         </Pressable>
-        <Image source={{uri: pokemon.data.sprites.other['official-artwork'].front_default}} style={styles.pokemonImage}/>
-        <Text style={styles.pokemonName}>{name}{`(#${pokemon.data.id})`}</Text>
-        <View>
+        <Image
+          source={{
+            uri: pokemon.data.sprites.other["official-artwork"].front_default,
+          }}
+          style={styles.pokemonImage}
+        />
+        <View style={styles.nameContainer}>
+          <Text style={styles.pokemonName}>
+            {name}
+            {`(#${pokemon.data.id})`}
+          </Text>
           <Pressable
             onPress={() => {
-              navigation.navigate('WebViewContainer')
+              navigation.navigate("WebViewContainer", {
+                url: `https://bulbapedia.bulbagarden.net/wiki/${pokemon.data.name}`,
+              });
             }}
             style={styles.pokeballButton}
           >
-            <Text>Click here</Text>
+            <Text>🔗</Text>
           </Pressable>
         </View>
       </View>
@@ -39,27 +60,30 @@ export const Pokemon = ({ route, navigation }) => {
   );
 };
 
-
 const styles = StyleSheet.create({
   pageContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   pokeballButton: {
     width: 40,
-    height: 40
+    height: 40,
   },
   pokeballImage: {
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
   },
   pokemonImage: {
     width: 200,
-    height: 200
+    height: 200,
   },
   pokemonName: {
     fontSize: fontSize.large,
     color: colors.black,
-    textTransform: 'capitalize',
-  }
+    textTransform: "capitalize",
+  },
+  nameContainer: {
+    flexDirection: "row",
+    gap: 12,
+  },
 });
--- src/screens/WebViewContainer.tsx
+++ src/screens/WebViewContainer.tsx
@@ -1,7 +1,27 @@
-import { WebView } from 'react-native-webview';
+import { useRoute } from "@react-navigation/native";
+import { useRef } from "react";
+import { WebView } from "react-native-webview";
 
 export const WebViewContainer = () => {
-  return(
-    <WebView source={{ uri: 'https://reactnative.dev/' }} />
-  )
-}
+  const { params } = useRoute();
+  const isInternal = params.url.includes("rn-workshop");
+  const webViewRef = useRef<WebView>(null);
+
+  return isInternal ? (
+    <WebView
+      source={{ uri: params.url }}
+      ref={webViewRef}
+      onLoadEnd={() => {
+        webViewRef.current?.postMessage(
+          JSON.stringify({
+            code: "capture_pokemons",
+            data: [],
+          })
+        );
+      }}
+      // onMessage={}
+    />
+  ) : (
+    <WebView source={{ uri: params.url }} />
+  );
+};
