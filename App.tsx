50fc562509c619c340eead8b373a24d654e7b098
Matheus Vinicius Valenza <matheus.valenza@Matheuss-MacBook-Pro.local>
2024-03-20 15:51:31 -0300

    adding react query and axios config

--- App.tsx
+++ App.tsx
@@ -5,20 +5,25 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Home } from "screens/Home";
 import { Pokedex } from "screens/Pokedex";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const Stack = createNativeStackNavigator();
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="Pokedex" component={Pokedex} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Pokedex" component={Pokedex} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
--- src/screens/Pokedex.tsx
+++ src/screens/Pokedex.tsx
@@ -1,6 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import { SafeAreaView, Text } from "react-native";
 
 export const Pokedex = () => {
+  // const { data, isFetching } = useQuery({queryKey: 'pokemons-list', queryFn: () => });
+
   return (
     <SafeAreaView>
       <Text>Pokedex screen</Text>
