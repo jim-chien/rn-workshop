06a1b50b3101a08df5d68b351836d39ca1385a2e
Jim Chien <jim_chien_br@hotmail.com>
2024-03-20 15:23:22 -0300

    started homepage

--- App.tsx
+++ App.tsx
@@ -1,21 +1,46 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Image,
+  Button,
+  View,
+  Pressable,
+} from "react-native";
+import { colors, fontSize } from "./theme";
+import Pokeball from "./assets/pokeball.png";
 
 export default function App() {
-  console.log("hi");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>React Native Pokedex</Text>
+      <Image source={Pokeball} />
+      <Pressable onPress={() => { }}>
+        <Text style={styles.buttonText}>GET STARTED</Text>
+      </Pressable>
+      {/* <View style={styles.button}> */}
+      {/*   <Button title="GET STARTED" onPress={() => { }} color={colors.white} /> */}
+      {/* </View> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.lightRed,
     alignItems: "center",
-    justifyContent: "center",
+  },
+  title: {
+    marginTop: 50,
+    fontSize: fontSize.extraLarge,
+    fontWeight: "bold",
+    color: colors.white,
+    textAlign: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: fontSize.medium,
+    color: colors.white,
   },
 });
